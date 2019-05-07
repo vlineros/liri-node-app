da5e5de52d7083f2638814624e83a8332c75fa98
vlineros <victorlineros@gmail.com>
2019-05-07 18:53:31 -0500

    created initial file structure

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+var spotify = new Spotify(keys.spotify);
+
+switch (process.argv[2]) {
+  case "concert-this":
+
+  case "spotify-this-song":
+
+  case "movie-this":
+
+  case "do-what-it-says":
+}
